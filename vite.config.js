19b805f6e0617a06c916bc59902052cb6bd06e60
ohdaridari <daria230t@gmail.com>
2023-10-18 08:43:34 +0200

    changed build destination

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+
+export default {
+  build: {
+    outDir: 'public', // Specify the output directory for the build
+  },
+  server: {
+    port: 3000, // Specify the port to use for the development server
+  },
+}
